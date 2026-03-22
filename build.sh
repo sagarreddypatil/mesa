fd5eaf308af282abbc1d887e2909287541a94bbf
Sagar Patil <sagarreddypatil@gmail.com>
2026-03-22 01:29:01 -0700

    shits working don't fw it

---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env arch -x86_64 bash
+
+rm -rf ~/mesa-native
+rm -rf ~/spirv-llvm-translator-21
+
+set -e
+
+export PATH="/usr/local/opt/llvm@21/bin:/usr/local/opt/bison/bin:$PATH"
+export SDKROOT=$(xcrun --show-sdk-path)
+
+cd ~/Documents/SPIRV-LLVM-Translator
+# rm -rf build
+
+cmake -S . -B build -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="$HOME/spirv-llvm-translator-21" \
+  -DLLVM_DIR="$(/usr/local/bin/brew --prefix llvm@21)/lib/cmake/llvm" \
+  -DCMAKE_PREFIX_PATH="$(/usr/local/bin/brew --prefix llvm@21)"
+
+cmake --build build
+cmake --install build
+
+cd ~/Documents/mesa
+rm -rf build
+export LLVM_CONFIG=/usr/local/opt/llvm@21/bin/llvm-config
+
+export PATH="/usr/local/opt/llvm@21/bin:/usr/local/opt/bison/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:$HOME/spirv-llvm-translator-21/lib/pkgconfig"
+export LDFLAGS="-L/usr/local/lib"
+export CPPFLAGS="-I/usr/local/include"
+
+export PKG_CONFIG_PATH="$HOME/spirv-llvm-translator-21/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CMAKE_PREFIX_PATH="$HOME/spirv-llvm-translator-21:$CMAKE_PREFIX_PATH"
+
+source .venv/bin/activate
+
+meson setup build --native-file native.ini \
+  -Dprefix=$HOME/mesa-native \
+  -Dbuildtype=release \
+  -Dplatforms=macos \
+  -Degl-native-platform=surfaceless \
+  -Degl=enabled \
+  -Dgallium-drivers=zink \
+  -Dvulkan-drivers=kosmickrisp \
+  -Dgles1=enabled \
+  -Dgles2=enabled \
+  -Dglx=disabled \
+  -Dgbm=disabled \
+  -Dmoltenvk-dir=/usr/local/opt/molten-vk
+
+ninja -C build
+ninja -C build install
+
+mkdir -p $HOME/mesa-native/lib/dri
+cd $HOME/mesa-native/lib/dri  
+ln -sf ../libgallium-26.0.0-devel.dylib zink_dri.so
+ln -sf ../libgallium-26.0.0-devel.dylib swrast_dri.so
+
+cp /usr/local/lib/libvulkan.1.dylib $HOME/mesa-native/lib/
